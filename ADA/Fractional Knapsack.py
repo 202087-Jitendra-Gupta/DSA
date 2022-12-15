c5847663c3786629ea92fa5a2ff867c5208750c5
Jitendra Gupta <100828866+202087-Jitendra-Gupta@users.noreply.github.com>
2022-12-15 20:10:14 +0530

    Update Fractional Knapsack.py

--- ADA/Fractional Knapsack.py
+++ ADA/Fractional Knapsack.py
@@ -1,48 +1,71 @@
-# Structure for an item which stores weight and
-# corresponding value of Item
-class Item:
-	def __init__(self, value, weight):
-		self.value = value
-		self.weight = weight
+// C++ program to solve fractional Knapsack Problem
 
-# Main greedy function to solve problem
-def fractionalKnapsack(W, arr):
+#include <bits/stdc++.h>
+using namespace std;
 
-	# sorting Item on basis of ratio
-	arr.sort(key=lambda x: (x.value/x.weight), reverse=True)
+// Structure for an item which stores weight and
+// corresponding value of Item
+struct Item {
+	int value, weight;
 
-	# Uncomment to see new order of Items with their
-	# ratio
-	# for item in arr:
-	#	 print(item.value, item.weight, item.value/item.weight)
+	// Constructor
+	Item(int value, int weight)
+	{
+		this->value = value;
+		this->weight = weight;
+	}
+};
 
-	# Result(value in Knapsack)
-	finalvalue = 0.0
+// Comparison function to sort Item
+// according to val/weight ratio
+bool cmp(struct Item a, struct Item b)
+{
+	double r1 = (double)a.value / (double)a.weight;
+	double r2 = (double)b.value / (double)b.weight;
+	return r1 > r2;
+}
 
-	# Looping through all Items
-	for item in arr:
+// Main greedy function to solve problem
+double fractionalKnapsack(int W, struct Item arr[], int N)
+{
+	// Sorting Item on basis of ratio
+	sort(arr, arr + N, cmp);
 
-		# If adding Item won't overflow, add it completely
-		if item.weight <= W:
-			W -= item.weight
-			finalvalue += item.value
+	double finalvalue = 0.0;
 
-		# If we can't add current Item, add fractional part
-		# of it
-		else:
-			finalvalue += item.value * W / item.weight
-			break
-	# Returning final value
-	return finalvalue
+	// Looping through all items
+	for (int i = 0; i < N; i++) {
+		
+		// If adding Item won't overflow,
+		// add it completely
+		if (arr[i].weight <= W) {
+			W -= arr[i].weight;
+			finalvalue += arr[i].value;
+		}
 
+		// If we can't add current Item,
+		// add fractional part of it
+		else {
+			finalvalue
+				+= arr[i].value
+				* ((double)W / (double)arr[i].weight);
+			break;
+		}
+	}
 
-# Driver's Code
-if __name__ == "__main__":
+	// Returning final value
+	return finalvalue;
+}
 
-	# Weight of Knapsack
-	W = 50
-	arr = [Item(60, 10), Item(100, 20), Item(120, 30)]
+// Driver code
+int main()
+{
+	int W = 50;
+	Item arr[] = { { 60, 10 }, { 100, 20 }, { 120, 30 } };
 
-	# Function call
-	max_val = fractionalKnapsack(W, arr)
-	print ('Maximum value we can obtain = {}'.format(max_val))
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	// Function call
+	cout << fractionalKnapsack(W, arr, N);
+	return 0;
+}
